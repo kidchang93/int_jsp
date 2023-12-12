4585aab351a7e8e43524e63caf1413b0f51f3bf9
kidchang93 <lck930721@gmail.com>
2023-12-12 14:54:15 +0900

    JSP에서 스프링 흉내내기

---
+++ jdbcex/src/main/java/org/zerock/jdbcex/dto/TodoDTO.java
@@ -0,0 +1,20 @@
+package org.zerock.jdbcex.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Builder
+@Data
+@NoArgsConstructor // 파라미터가 없는 생성자
+@AllArgsConstructor // 파라미터가 모두 있는 생성자
+public class TodoDTO {
+  private Long tno;
+  private String title;
+  private LocalDate dueDate;
+  private boolean finished;
+}
