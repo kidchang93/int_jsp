001a736ef704b4147b3dc5067f4466d116cdfad8
kidchang93 <lck930721@gmail.com>
2023-12-12 11:17:01 +0900

    인텔리제이 스프링 환경설정인줄 알았으나 JSP 에서 스프링처럼 하는 것

---
+++ jdbcex/src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -0,0 +1,17 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.zerock.jdbcex.dao.TodoDAO;
+
+public class TodoDAOTests {
+  private TodoDAO todoDAO;
+  @BeforeEach
+  public void ready(){
+    todoDAO = new TodoDAO();
+  }
+  @Test
+  public void testTime() throws Exception{
+    System.out.println("testTime 결과 =======> "+todoDAO.getTime());
+  }
+}
