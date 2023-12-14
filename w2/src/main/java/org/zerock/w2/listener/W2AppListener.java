7c07ada71a727da4bfa65d99e11fe5d995cbb0e1
kidchang93 <lck930721@gmail.com>
2023-12-14 11:18:43 +0900

    docs : servlet context 이용

--- w2/src/main/java/org/zerock/w2/listener/W2AppListener.java
+++ w2/src/main/java/org/zerock/w2/listener/W2AppListener.java
@@ -2,6 +2,7 @@ package org.zerock.w2.listener;
 
 import lombok.extern.log4j.Log4j2;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -15,6 +16,10 @@ public class W2AppListener implements ServletContextListener {
     log.info("--------------init-----------------");
     log.info("--------------init-----------------");
     log.info("--------------init-----------------");
+
+    ServletContext servletContext = sce.getServletContext();
+
+    servletContext.setAttribute("appName", "W2");
 
   }
 
