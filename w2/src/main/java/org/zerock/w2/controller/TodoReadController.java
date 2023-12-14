aa16eef6e994a9a4d7e9131c33f704a43e84deee
kidchang93 <lck930721@gmail.com>
2023-12-14 10:31:43 +0900

    쿠키 생성하고 저장하기 f12 눌러 확인해보기

--- w2/src/main/java/org/zerock/w2/controller/TodoReadController.java
+++ w2/src/main/java/org/zerock/w2/controller/TodoReadController.java
@@ -38,12 +38,40 @@ private TodoService todoService = TodoService.INSTANCE;
         exist = true;
       }
 
+      log.info("exist : " + exist);
 
-
+      if (!exist) {
+        todoListStr += tno + "-";
+        viewTodoCookie.setValue(todoListStr);
+        viewTodoCookie.setMaxAge(60 * 60 * 24);
+        viewTodoCookie.setPath("/");
+        resp.addCookie(viewTodoCookie);
+      }
       req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req,resp);
+
     } catch (Exception e){
       log.error(e.getMessage());
       throw new ServletException("read error");
     }
+  }
+
+  private Cookie findCookie (Cookie[] cookies, String cookieName){
+
+    Cookie targetCookie = null;
+
+    if (cookies != null && cookies.length > 0 ) {
+      for (Cookie ck : cookies){
+        if (ck.getName().equals(cookieName)){
+          targetCookie = ck;
+          break;
+        }
+      }
+    }
+    if (targetCookie == null) {
+      targetCookie = new Cookie(cookieName , "");
+      targetCookie.setPath("/");
+      targetCookie.setMaxAge(60*60*24);
+    }
+    return targetCookie;
   }
 }
